edbf2f144a1ba82e6ac338aa60719a2256d48379
franklant <frankboss349@gmail.com>
2026-06-25 16:32:09 -0400

    add: simple rectangle movmenet and bounds checking

--- src/constants.h
+++ src/constants.h
@@ -1,4 +1,4 @@
 namespace Constants {
-    const int WINDOW_WIDTH = 500;
-    const int WINDOW_HEIGHT = 500;
+    const int WINDOW_WIDTH = 1000;
+    const int WINDOW_HEIGHT = 1000;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,102 @@
 #include <SFML/Graphics.hpp>
 #include "constants.h"
 
+
+/// @brief Modifes the position of the shape using reference. Instead of assigning a variable
+/// to the new return value reflecting the change, we can modify the original variable by
+/// reference.
+/// @param r The rectangle we want to move.
+/// @param spd The speed at which we want to move the rectangle.
+void moveShapeByRef(sf::RectangleShape &r, float spd)
+{
+	// move to the left
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x - spd,
+			r.getPosition().y - 0
+		));
+	}
+
+	// move to the right
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x + spd,
+			r.getPosition().y - 0
+		));
+	}
+
+	// move up
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x - 0,
+			r.getPosition().y - spd
+		));
+	}
+
+	// move down
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x - 0,
+			r.getPosition().y + spd
+		));
+	}
+}
+
+void keepInBoundsByRef(sf::RectangleShape &r)
+{
+	// if crossing the right border
+	if (r.getPosition().x >= Constants::WINDOW_WIDTH)
+	{
+		r.setPosition(sf::Vector2f(
+			0 - r.getSize().x,
+			r.getPosition().y
+		));
+	}
+
+	// if crossing the left border
+	if (r.getPosition().x + r.getSize().x <= -0.5)
+	{
+		r.setPosition(sf::Vector2f(
+			Constants::WINDOW_WIDTH,
+			r.getPosition().y
+		));
+	}
+
+	// if crossing the bottom border
+	if (r.getPosition().y >= Constants::WINDOW_HEIGHT)
+	{
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x,
+			0 - r.getSize().y
+		));
+	}
+
+	// if crossing the top border
+	if (r.getPosition().y + r.getSize().y <= -0.5)
+	{
+		r.setPosition(sf::Vector2f(
+			r.getPosition().x,
+			Constants::WINDOW_HEIGHT
+		));
+	}
+}
+
 int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { Constants::WINDOW_WIDTH, Constants::WINDOW_HEIGHT } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	
+	// create the rectangle
+	sf::Vector2f rectSize(20.0f, 20.0f);
+	sf::RectangleShape rect(rectSize);
+
+	// set the color and position of rectangle
+	rect.setFillColor(sf::Color::Blue);
+	rect.setPosition(sf::Vector2f(
+		(Constants::WINDOW_WIDTH / 2.0f) - (rectSize.x / 2.0f),
+		(Constants::WINDOW_HEIGHT / 2.0f) - (rectSize.y / 2.0f)
+	));
+	
+	float speed = 0.1f;
 
 	// EVENT LOOP : while the window is open
 	while ( window.isOpen() )
@@ -17,7 +108,11 @@ int main()
 		}
 
 		window.clear();
-		window.draw( shape );
+
+		moveShapeByRef(rect, speed);
+		keepInBoundsByRef(rect);
+
+		window.draw( rect );
 		window.display();
 	}
 }
